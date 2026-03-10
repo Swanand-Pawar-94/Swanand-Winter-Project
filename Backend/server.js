0db7eeb1d7c4d890245131897f1d34369178cd98
Swanand Prashant Pawar <swanandpawar21@gmail.com>
2026-03-10 17:05:34 +0530

    Prepare for Render deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,9 @@
+const fs = require('fs');
+const dataDir = path.join(__dirname, '../data');
+if (!fs.existsSync(dataDir)) {
+  fs.mkdirSync(dataDir, { recursive: true });
+}
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
